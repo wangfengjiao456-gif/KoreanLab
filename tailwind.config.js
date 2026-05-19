8e97cf09269f411d8238ae8e98cbc398f5825d32
wangfengjiao456-gif <wangfengjiao456@gmail.com>
2026-05-19 19:31:18 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+content: ['./index.html', './*.{js,jsx}', './src/**/*.{js,jsx}'],
+
   theme: { extend: {} },
   plugins: [],
 }
